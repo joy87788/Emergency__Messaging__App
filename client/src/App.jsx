5afafe1b5d31c9104954a3efbc16c67456877be7
mohajon33 <mohajon33@gmail.com>
2021-11-02 00:44:07 +0600

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelListContainer, ChannelContainer } from "./components";
+
+import "./App.css";
+
+const apikey = "p8swxpg57mxr";
+const client = StreamChat.getInstance(apikey);
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
